e4bc046a0e059b763275201a9d89d0a6262b9cff
GEON AN <2022162023@stuoffice.eulji.ac.kr>
2025-05-17 19:33:00 +0900

    init vue3 web

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold text-blue-600">Tailwind 적용 완료 🎉</h1>
+</template>
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
